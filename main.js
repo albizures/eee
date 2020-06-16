898e6bd64773c4769f72e4c8e59a0f8a6820fb83
Jose Albizures <albizures3601@gmail.com>
2020-06-16 21:31:33 +0300

    change size

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const widthOffset = 200;
 function createWindow() {
 	const [mainScreen] = screen.getAllDisplays();
 	const x = widthOffset / 2;
+	const width = mainScreen.size.width - widthOffset;
 	const win = new BrowserWindow({
 		width: mainScreen.size.width - widthOffset,
 		height,
@@ -24,6 +25,18 @@ function createWindow() {
 	});
 	win.setAlwaysOnTop(true, 'floating');
 	win.loadFile('index.html');
+
+	app.on('browser-window-focus', () => {
+		if (win) {
+			win.setSize(width, height);
+		}
+	});
+
+	app.on('browser-window-blur', () => {
+		if (win) {
+			win.setSize(1, 1);
+		}
+	});
 }
 
 app.whenReady().then(createWindow);
